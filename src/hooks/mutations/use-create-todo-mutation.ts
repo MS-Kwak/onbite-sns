57436fe5fc48cca2a7d48c830e7b8a0029f73cb3
MS-Kwak <kkminseo@gmail.com>
2025-12-10 20:47:13 +0900

    29. (4.8) 캐시 데이터 다루기 2 - 응답 결과값 이용하기

--- src/hooks/mutations/use-create-todo-mutation.ts
+++ src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,5 +1,6 @@
 import { createTodo } from "@/api/create-todo";
 import { QUERY_KEYS } from "@/lib/constants";
+import type { Todo } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreateTodoMutation() {
@@ -9,9 +10,11 @@ export function useCreateTodoMutation() {
     mutationFn: createTodo,
     onMutate: () => {},
     onSettled: () => {},
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
+    onSuccess: (newTodo) => {
+      // 캐시 데이터의 리페치 없이 직접 데이터를 업데이트 합니다.
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [newTodo];
+        return [...prevTodos, newTodo];
       });
     },
     onError: (error) => {
